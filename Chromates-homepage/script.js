e5fc9c09462673065596aa7f17b6766262d4099f
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-04 21:11:09 +0900

    Update script.js

--- Chromates-homepage/script.js
+++ Chromates-homepage/script.js
@@ -1,3 +1,37 @@
 // JavaScript template using Windows Script Host
 var WSHShell = WScript.CreateObject("WScript.Shell");
 WSHShell.Popup( "Hello World!" );
+
+const newsList = document.getElementById("news-list");
+
+if (newsList) {
+
+  if (newsData.length === 0) {
+
+    newsList.innerHTML = `
+      <p class="no-news">
+        現在お知らせはありません。
+      </p>
+    `;
+
+  } else {
+
+    newsData.slice(0, 4).forEach(news => {
+
+      const item = document.createElement("a");
+
+      item.href = news.url;
+      item.className = "news-item";
+
+      item.innerHTML = `
+        <time>${news.date}</time>
+        <span>${news.title}</span>
+      `;
+
+      newsList.appendChild(item);
+
+    });
+
+  }
+
+}
