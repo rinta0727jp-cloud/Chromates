33e6b9e63783e36cdca884e204701046b581e823
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-05 17:30:44 +0900

    Update goods-data.js

--- Chromates-homepage/goods-data.js
+++ Chromates-homepage/goods-data.js
@@ -1,1 +1,147 @@
+const goodsData = [
 
+  {
+    id: "uniform",
+    name: "OFFICIAL UNIFORM",
+    jpName: "ChroMates 公式ユニフォーム",
+    price: "¥8,800",
+    image: "images/goods/uniform.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "acrylic-keyholder",
+    name: "ACRYLIC KEYHOLDER",
+    jpName: "アクリルキーホルダー",
+    price: "¥900",
+    image: "images/goods/acrylic_keyholder.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "uniform-keyholder",
+    name: "UNIFORM KEYHOLDER",
+    jpName: "ユニフォームキーホルダー",
+    price: "¥1,200",
+    image: "images/goods/uniform_keyholder.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "plush",
+    name: "LOSERSAURUS PLUSH",
+    jpName: "LOSERSAURUS ぬいぐるみ",
+    price: "¥3,800",
+    image: "images/goods/plush.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "sticker-set",
+    name: "STICKER SET",
+    jpName: "ステッカーセット",
+    price: "¥600",
+    image: "images/goods/sticker_set.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "rubber-keyholder",
+    name: "RUBBER KEYHOLDER",
+    jpName: "ラバーキーホルダー",
+    price: "¥1,000",
+    image: "images/goods/rubber_keyholder.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "muffler-towel",
+    name: "MUFFLER TOWEL",
+    jpName: "マフラータオル",
+    price: "¥2,200",
+    image: "images/goods/muffler_towel.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "tshirt",
+    name: "OFFICIAL T-SHIRT",
+    jpName: "オフィシャルTシャツ",
+    price: "¥3,500",
+    image: "images/goods/tshirt.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "cap",
+    name: "OFFICIAL CAP",
+    jpName: "オフィシャルキャップ",
+    price: "¥3,300",
+    image: "images/goods/cap.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "tote-bag",
+    name: "TOTE BAG",
+    jpName: "トートバッグ",
+    price: "¥2,200",
+    image: "images/goods/tote_bag.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "smartphone-case",
+    name: "SMARTPHONE CASE",
+    jpName: "スマホケース",
+    price: "¥2,800",
+    image: "images/goods/smartphone_case.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "acrylic-stand",
+    name: "ACRYLIC STAND",
+    jpName: "アクリルスタンド",
+    price: "¥1,500",
+    image: "images/goods/acrylic_stand.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "can-badge",
+    name: "CAN BADGE",
+    jpName: "缶バッジ",
+    price: "¥500",
+    image: "images/goods/can_badge.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "uniform-magnet",
+    name: "UNIFORM MAGNET",
+    jpName: "ユニフォームマグネット",
+    price: "¥900",
+    image: "images/goods/uniform_magnet.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "wristband",
+    name: "WRISTBAND",
+    jpName: "リストバンド",
+    price: "¥1,200",
+    image: "images/goods/wristband.jpg",
+    status: "PRE-ORDER"
+  },
+
+  {
+    id: "mini-flag",
+    name: "MINI FLAG",
+    jpName: "ミニフラッグ",
+    price: "¥1,500",
+    image: "images/goods/mini_flag.jpg",
+    status: "PRE-ORDER"
+  }
+
+];
