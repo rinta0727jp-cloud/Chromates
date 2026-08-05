b3a1541b90030032343c35c366cb1306a0d20e0b
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-05 20:51:20 +0900

    Update schedule-data.js

--- Chromates-homepage/schedule-data.js
+++ Chromates-homepage/schedule-data.js
@@ -1,1 +1,65 @@
 const scheduleData = [];
+
+// =========================
+// NEXT ACTIVITY
+// =========================
+
+const nextActivity =
+  document.getElementById("next-activity");
+
+if (nextActivity) {
+
+  if (scheduleData.length === 0) {
+
+    nextActivity.innerHTML = `
+      <p class="next-label">NEXT ACTIVITY</p>
+
+      <div class="next-date">
+        TBA
+      </div>
+
+      <div class="next-line"></div>
+
+      <strong>COMING SOON</strong>
+
+      <p>次回の活動日は現在調整中です。</p>
+
+      <a href="#schedule" class="outline-button">
+        SCHEDULE →
+      </a>
+    `;
+
+  } else {
+
+    const next = scheduleData[0];
+
+    const date = new Date(next.date);
+
+    const month =
+      String(date.getMonth() + 1).padStart(2, "0");
+
+    const day =
+      String(date.getDate()).padStart(2, "0");
+
+    nextActivity.innerHTML = `
+      <p class="next-label">NEXT ACTIVITY</p>
+
+      <div class="next-date">
+        ${month}.${day}
+        <span>${next.day}</span>
+      </div>
+
+      <div class="next-line"></div>
+
+      <strong>${next.time}</strong>
+
+      <p>${next.place}</p>
+
+      <a href="#schedule" class="outline-button">
+        詳しく見る →
+      </a>
+    `;
+
+  }
+
+}
