6bc51d3154766a6db4ead0f380308201dba9073f
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-09 07:32:20 +0900

    Update gallery-data.js

--- Chromates-homepage/gallery-data.js
+++ Chromates-homepage/gallery-data.js
@@ -1,5 +1,13 @@
 const galleryData = [
   {
+  image: "images/baseball-game.jpg",
+  alt: "ChroMates野球部門 所沢大会の試合風景"
+},
+{
+  image: "images/baseball-team.jpg",
+  alt: "ChroMates野球部門 所沢大会終了後の集合写真"
+},
+  {
     date: "2026.08.04",
     title: "ChroMates",
     image: "images/team.jpg",
