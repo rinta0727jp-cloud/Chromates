0c42a01614117417019a3b102cb17c8d6b0a65fb
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-09 06:56:34 +0900

    Update news-data.js

--- Chromates-homepage/news-data.js
+++ Chromates-homepage/news-data.js
@@ -1,4 +1,9 @@
 const newsData = [
+   {
+    date: "2026.08.09",
+    title: "野球部門 所沢大会に参加しました",
+    url: "news/baseball-tokorozawa.html"
+  },
   {
     date: "2026.08.04",
     title: "ChroMates公式ホームページを公開しました！",
