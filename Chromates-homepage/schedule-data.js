7770b1d568407db6939a1a2868f80c0ed7321774
rinta0727jp-cloud <rinta0727jp@gmail.com>
2026-08-09 06:31:02 +0900

    Update schedule-data.js

--- Chromates-homepage/schedule-data.js
+++ Chromates-homepage/schedule-data.js
@@ -1,3 +1,11 @@
 const scheduleData = [
   // 予定を追加するときはここに書く
+  {
+    date: "2026-08-07",
+    day: "FRI",
+    type: "EVENT",
+    title: "野球部 所沢大会",
+    time: "15:00 - 19:00",
+    place: "総合運動場"
+  }
 ];
